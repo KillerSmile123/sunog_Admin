0e0c1894c2d654799fe549d1337a10f1b527707e
Mark Godwill <markgodwill15@gmail.com>
2026-01-01 01:45:56 +0800

    changes

--- js/admin.js
+++ js/admin.js
@@ -56,13 +56,14 @@ class AdminLogin {
     this.setLoadingState(submitButton, true);
 
     try {
-      const response = await fetch('http://localhost:8000/login', {
-        method: 'POST',
-        headers: { 
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
+      const response = await fetch('http://127.0.0.1:5000/login', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+});
+
 
       const data = await response.json();
 
